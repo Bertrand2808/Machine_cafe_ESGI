85ab913ed59c92f8b4818f329a5705758bc023f1
Bertrand RENAUDIN <bertrand.renaudin@odigo.com>
2022-10-10 13:24:41 +0200

    Cafe 10_10_2022

---
+++ Machine_a_cafe/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Bienvenue();
+        
+    }
+
+    private static void Bienvenue() {
+        // Affichage de bienvenue
+        System.out.println();
+        System.out.println();
+        System.out.println("Bienvenue dans votre service de machine à café interactive !");
+        System.out.println();
+        System.out.println("*************************************************************");
+        System.out.println("*  ***********    ***********   ***********   ***********   *");
+        System.out.println("*  *              *         *   *             *             *");
+        System.out.println("*  *              *         *   *             *             *");
+        System.out.println("*  *              ***********   *******       ***********   *");
+        System.out.println("*  *              *         *   *             *             *");
+        System.out.println("*  *              *         *   *             *             *");
+        System.out.println("*  ***********    *         *   *             ***********   *");
+        System.out.println("*************************************************************");
+        System.out.println("");
+        System.out.println("Veuillez faire votre choix parmis les produits disponibles : ");
+        System.out.println();
+    }
+
+}
