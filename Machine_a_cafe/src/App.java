2f4600445e58ba6d17459d68b4d96f97ef4f42aa
Bertrand RENAUDIN <bertrand.renaudin@odigo.com>
2022-10-10 14:34:33 +0200

    1ere modifications

--- Machine_a_cafe/src/App.java
+++ Machine_a_cafe/src/App.java
@@ -1,7 +1,96 @@
+import java.util.Scanner;
+
 public class App {
+    static Scanner scanner = new Scanner(System.in);
+    static int choixMenu = 0;
+    static String produit;
     public static void main(String[] args) throws Exception {
         Bienvenue();
+        Menu();
+
         
+    }
+
+    private static void Sucre() {
+        System.out.println();
+        System.out.println("Souhaitez vous du sucre dans votre '"+produit+"' ?" );
+        System.out.println();
+        System.out.println("****************************************************************");
+    }
+
+    public static void Menu() {
+        int choix=0;
+        System.out.println("1- Café court ");
+        System.out.println("2- Café long ");
+        System.out.println("3- Expresso ");
+        System.out.println("4- Café court en grain ");
+        System.out.println("5- Café long en grain ");
+        System.out.println("6- Cappuccino ");
+        System.out.println();
+        
+        try {
+            System.out.print(" Entrer votre choix : ");
+            choix = Integer.parseInt(scanner.nextLine());
+            System.out.println();
+            System.out.println("****************************************************************");
+            System.out.println();
+        } catch (Exception e) {
+            System.out.println("Veuillez entrer un nombre correct");
+        }
+        
+        switch(choix){
+
+            case 1:
+            choixMenu = choix;
+            produit = "Cafe court";
+            System.out.println(" Vous avez séléctionné : "+ produit);
+            System.out.println();
+            System.out.println("****************************************************************");
+            Sucre();
+            break;
+            case 2:
+            choixMenu = choix;
+            produit = "Cafe long";
+            System.out.println(" Vous avez séléctionné : "+ produit);
+            System.out.println();
+            System.out.println("****************************************************************");
+            Sucre();
+            break;
+            case 3:
+            choixMenu = choix;
+            produit = "Expresso";
+            System.out.println(" Vous avez séléctionné : "+ produit);
+            System.out.println();
+            System.out.println("****************************************************************");
+            Sucre();
+            break;
+            case 4:
+            choixMenu = choix;
+            produit = "Cafe court en grain";
+            System.out.println(" Vous avez séléctionné : "+ produit);
+            System.out.println();
+            System.out.println("****************************************************************");
+            Sucre();
+            break;
+            case 5:
+            choixMenu = choix;
+            produit = "Cafe long en grain";
+            System.out.println(" Vous avez séléctionné : "+ produit);
+            System.out.println();
+            System.out.println("****************************************************************");
+            Sucre();
+            break;
+            case 6:
+            choixMenu = choix;
+            produit = "Cappuccino";
+            System.out.println(" Vous avez séléctionné : "+ produit);
+            System.out.println();
+            System.out.println("****************************************************************");
+            Sucre();
+            break;
+
+
+        }
     }
 
     private static void Bienvenue() {
